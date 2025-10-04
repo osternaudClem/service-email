a18252665c8dee66a50188cd99583317b0ded644
Cletus <osternaud.clement@gmail.com>
2025-10-04 13:38:09 +0200

    fix: add x-api-key to allowed headers in CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: ["https://azelysse.fr", "http://localhost:3000"],
     allowMethods: ["GET", "POST", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
+    allowHeaders: ["Content-Type", "Authorization", "x-api-key"],
   })
 );
 
